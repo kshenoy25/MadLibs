dc144666078c2e7a490b37138abc1e2197226d91
KSCode <kunalshenoy25@gmail.com>
2024-10-23 13:46:34 -0500

    MadLibs Project

---
+++ PythonMadLibs/MadLibsExamples/madlibs.py
@@ -0,0 +1,11 @@
+# string concatenation (aka how to put strings together)
+
+adj = input("Enter a Adjective: ")
+verb1 = input("Enter a Verb: ")
+verb2 = input("Enter another Verb: ")
+famous_person = input("Enter a famous person: ")
+
+madlib = (f"Computer programming is so {adj}! It makes me excited to code and solve problems because\n"
+          f"I love to {verb1}. Stay frosty and {verb2} like you are {famous_person}")
+
+print(madlib)
